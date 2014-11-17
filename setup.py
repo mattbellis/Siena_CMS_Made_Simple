9ce1fb11a3d0e1e4b4d5250bf2588acaf9fe3fe0
mattbellis <matthew.bellis@gmail.com>
2014-11-17 10:59:15 -0500

    First add of all of these tools.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='siena_cms_tools',
+    version='1.0',
+    description='Tools to interface with simplified CMS data.',
+    author='Matt Bellis',
+    author_email='matthew.bellis@gmail.com',
+    url='https://github.com/mattbellis/Siena_CMS_Made_Simple',
+    packages=['siena_cms_tools'],
+    )
---
+++ siena_cms_tools/__init__.py
@@ -0,0 +1,18 @@
+"""
+requests
+~~~~~~~~
+
+:copyright: (c) 2012 by Kenneth Reitz.
+:license: ISC, see LICENSE for more details.
+
+"""
+
+__title__ = 'siena_cms_tools'
+__version__ = '0.01.0'
+__build__ = 0x001100
+__author__ = 'Matt Bellis'
+__license__ = 'XXX'
+__copyright__ = 'XXX'
+
+
+from .siena_cms_tools import *
