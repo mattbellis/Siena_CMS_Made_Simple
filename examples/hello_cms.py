8e8a8d97d459e03f8cacd101ac80b6f51a41e6e5
mattbellis <matthew.bellis@gmail.com>
2015-03-31 13:57:34 -0400

    Added axis labels to hello_cms.

--- examples/hello_cms.py
+++ examples/hello_cms.py
@@ -5,18 +5,25 @@ import sys
 
 import time
 
+#import seaborn as sn
+
 start = time.time()
 collisions = cms.get_collisions(sys.argv[1])
 print "Time to read in %d collisions: %f seconds" % (len(collisions),time.time()-start)
 
 print len(collisions)
 
-#fig = plt.figure(figsize=(7,5),dpi=100)
-#ax = fig.add_subplot(1,1,1)
-#ax = fig.gca(projection='3d')
-#plt.subplots_adjust(top=0.98,bottom=0.02,right=0.98,left=0.02)
+fig = plt.figure(figsize=(7,5),dpi=100)
+ax = fig.add_subplot(1,1,1)
+ax = fig.gca(projection='3d')
 
-lines,fig,ax = cms.display_collision3D(collisions[1])
+lines,fig,ax = cms.display_collision3D(collisions[1],fig=fig,ax=ax)
 
-#plt.show(block=False)
+ax.set_xlabel(r'$p_x$ (GeV/c$^2$)',fontsize=16)
+ax.set_ylabel(r'$p_y$ (GeV/c$^2$)',fontsize=16)
+ax.set_zlabel(r'$p_z$ (GeV/c$^2$)',fontsize=16)
+
+plt.tight_layout()
+
+plt.show()
 
